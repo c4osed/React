5660e0ee15be191d87389617a89eb7ae8104e965
C4osed <c4osed@outlook.com>
2018-01-30 12:49:00 +0700

    navbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'
+import Navbar from './Navbar';
 
 const element = <div>
+    <Navbar/>
     <h1>Hello React</h1>
 </div>
 
