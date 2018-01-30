cef2240594e0a5532567802cb63acaee7de7ef5d
C4osed <c4osed@outlook.com>
2018-01-30 15:12:36 +0700

    install jquery and popper.js

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Bootstrap npm
 
+## install bootstrap
 ```
 npm install bootstrap
 ```
 
+## import bootstrap
+```
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+```
+
+## install jquery
+```
+npm install jquert
+```
+
+## install popper.js
+```
+npm install popper.js
+```
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import Navbar from './Navbar';
 
 const element = <div>
+    <Navbar/>
     <h1>Hello React</h1>
 </div>
 
